c1aec8d2708e556d8f0f5cffd729c4288fc40561
Vincent Pretre <vincent.pretre@gmail.com>
2016-02-24 16:28:57 +0100

    updated README and tests

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # hps-ruby-rspec
+[![Build Status](https://travis-ci.org/hiptest/hps-ruby-rspec.svg?branch=master)](https://travis-ci.org/hiptest/hps-ruby-rspec)
+
 Hiptest publisher samples with Ruby/Rspec
+
+In this repository you'll find tests generated in Ruby/Rspec format from [Hiptest](https://hiptest.net), using [Hiptest publisher](https://github.com/hiptest/hiptest-publisher).
+
+The goals are:
+
+ * to show how tests are exported in Ruby/Rspec.
+ * to check exports work out of the box (well, with implemented actionwords)
+
+System under test
+------------------
+
+The SUT is a (not that much) simple coffee machine. You start it, you ask for a coffee and you get it, sometimes. But most of times you have to add water or beans, empty the grounds. You have an automatic expresso machine at work or at home? So you know how it goes :-)
+
+Update tests
+-------------
+
+
+To update the tests:
+
+    hiptest-publisher -c rspec.conf --only=tests
+
+The tests are generated in [``spec/project_spec.rb``](https://github.com/hiptest/hps-ruby-rspec/blob/master/spec/project_spec.rb)
+
+Run tests
+---------
+
+
+To build the project and run the tests, use the following command:
+
+    bundle exec rspec --format RspecJunitFormatter -o report.xml
+
+The SUT implementation can be seen in [``src/coffee_machine.rb``](https://github.com/hiptest/hps-ruby-rspec/blob/master/src/coffee_machine.rb)
+
+The test report is generated in ```report.xml```
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,29 +2,67 @@
 require 'spec_helper'
 require_relative 'actionwords'
 
-describe 'CoffeeMachineHiptestPublisherSample' do
+describe 'Coffee machine - Hiptest publisher sample' do
   include Actionwords
 
-  context "Simple use" do
-    def simple_use(lang, ready_message)
-      # Given I start the coffee machine "<lang>"
-      i_start_the_coffee_machine(lang)
-      # When I take a coffee
-      i_take_a_coffee
-      # Then coffee should be served
-      coffee_should_be_served
-    end
-
-    it "English (uid:367ab985-2090-4051-a8c0-5859a98f597a)" do
-      simple_use('en', 'Ready')
-    end
-
-    it "French (uid:79ab98fc-a5b2-45af-afd5-c2f0be90648d)" do
-      simple_use('fr', 'Pret')
-    end
+  it "Simple use" do
+    # Well, sometimes, you just get a coffee.
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should be served
+    coffee_should_be_served
   end
 
-  it "Full grounds does not block coffee (uid:268fb815-e51b-4bbf-9ac0-319c0262adea)" do
+  it "Water runs away" do
+    # Simple scenario showing that after 50 coffees, the "Fill tank" message is displayed but it is still possible to have coffee until the tank is fully empty.
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When fifty coffees have been taken without filling the tank
+    fifty_coffees_have_been_taken_without_filling_the_tank
+    # Then message "Fill tank" should be displayed
+    message_message_should_be_displayed("Fill tank")
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should be served
+    coffee_should_be_served
+    # When I take "10" coffees
+    i_take_coffee_number_coffees(10)
+    # Then coffee should not be served
+    coffee_should_not_be_served
+    # And message "Fill tank" should be displayed
+    message_message_should_be_displayed("Fill tank")
+    # When I fill the water tank
+    i_fill_the_water_tank
+    # Then message "Ready" should be displayed
+    message_message_should_be_displayed("Ready")
+  end
+
+  it "Beans run out" do
+    # Simple scenario showing that after 38 coffees, the message "Fill beans" is displayed but it is possible to take two coffees until there is no more beans.
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When thirty eight coffees are taken without filling beans
+    thirty_eight_coffees_are_taken_without_filling_beans
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Fill beans" should be displayed
+    message_message_should_be_displayed("Fill beans")
+    # When I take "2" coffees
+    i_take_coffee_number_coffees(2)
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Fill beans" should be displayed
+    message_message_should_be_displayed("Fill beans")
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should not be served
+    coffee_should_not_be_served
+  end
+
+  it "Full grounds does not block coffee" do
+    # You keep getting coffee even if the "Empty grounds" message is displayed. That said it's not a fantastic idea, you'll get ground everywhere when you'll decide to empty it.
     # Given the coffee machine is started
     the_coffee_machine_is_started
     # When I take "29" coffees
@@ -49,68 +87,25 @@ describe 'CoffeeMachineHiptestPublisherSample' do
     message_message_should_be_displayed("Empty grounds")
   end
 
-  it "Water runs away (uid:a2a3fd8a-9662-4c4d-a505-6857c811b32e)" do
-    # Given the coffee machine is started
-    the_coffee_machine_is_started
-    # When fifty coffees have been taken without filling the tank
-    fifty_coffees_have_been_taken_without_filling_the_tank
-    # Then message "Fill tank" should be displayed
-    message_message_should_be_displayed("Fill tank")
-    # When I take a coffee
-    i_take_a_coffee
-    # Then coffee should be served
-    coffee_should_be_served
-    # When I take "10" coffees
-    i_take_coffee_number_coffees(10)
-    # Then coffee should not be served
-    coffee_should_not_be_served
-    # And message "Fill tank" should be displayed
-    message_message_should_be_displayed("Fill tank")
-    # When I fill the water tank
-    i_fill_the_water_tank
-    # Then message "Ready" should be displayed
-    message_message_should_be_displayed("Ready")
-  end
-
-  it "Beans run out (uid:09f21a05-c841-4798-b5c6-c2f8f53d4b7b)" do
-    # Given the coffee machine is started
-    the_coffee_machine_is_started
-    # When thirty eight coffees are taken without filling beans
-    thirty_eight_coffees_are_taken_without_filling_beans
-    # Then coffee should be served
-    coffee_should_be_served
-    # And message "Fill beans" should be displayed
-    message_message_should_be_displayed("Fill beans")
-    # When I take "2" coffees
-    i_take_coffee_number_coffees(2)
-    # Then coffee should be served
-    coffee_should_be_served
-    # And message "Fill beans" should be displayed
-    message_message_should_be_displayed("Fill beans")
-    # When I take a coffee
-    i_take_a_coffee
-    # Then coffee should not be served
-    coffee_should_not_be_served
-  end
-
   context "Messages are based on language" do
     def messages_are_based_on_language(lang, ready_message)
+      # Well, sometimes, you just get a coffee.
       # When I start the coffee machine "<lang>"
       i_start_the_coffee_machine(lang)
       # Then message "<ready_message>" should be displayed
       message_message_should_be_displayed(ready_message)
     end
 
-    it "English (uid:c5922e82-0ba6-4094-9572-7cd5134129c9)" do
+    it "English" do
       messages_are_based_on_language('en', 'Ready')
     end
 
-    it "French (uid:eeb2239b-6a5a-4c0a-9877-d3388a12d83e)" do
+    it "French" do
       messages_are_based_on_language('fr', 'Pret')
     end
   end
 
-  it "No messages are displayed when machine is shut down (uid:9674a4b2-c6f2-4c82-9d8e-f4515fff395e)" do
+  it "No messages are displayed when machine is shut down" do
     # Given the coffee machine is started
     the_coffee_machine_is_started
     # When I shutdown the coffee machine
